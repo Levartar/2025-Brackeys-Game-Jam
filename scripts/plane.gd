7eb604062abcec780879b02cd75d4c69d62b6d9e
Fabian Thome <thome.fabian@web.de>
2025-08-25 01:59:22 +0200

    updated trails

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -2,48 +2,47 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var rotation_speed = 1.5
-@export var cool_down = 2.0
+@export var cooldown = 1.5
 
 var rotation_direction = 0
 var deploying: bool = false
 var last_deployment_state: bool = false
 var trail: Line2D
-var trail_scene: PackedScene = preload("res://scenes/Trail.tscn")
+var trail_scene: PackedScene = preload("res://scenes/trail.tscn")
 var latest_trail: Node
 
 var cooling_down: bool = false
 var current_cool_down: float = 0.0
+var deployed_after_cooldown: bool = false
 
 func _ready() -> void:
-  current_cool_down = cool_down
-
-func get_input():
-  rotation_direction = Input.get_axis("left", "right")
-  velocity = transform.y * -1 * speed
-  deploying = Input.is_action_just_pressed("deploy")
+  current_cool_down = cooldown
 
 func _process(delta: float) -> void:
   if cooling_down:
     current_cool_down -= delta
     if current_cool_down <= 0:
       cooling_down = false
-      current_cool_down = cool_down
+      current_cool_down = cooldown
+      $Sprite2D.modulate = Color(1, 1, 1, 1)
+      deployed_after_cooldown = false
   else:
-    if deploying:
+    if Input.is_action_just_pressed("deploy"):
       latest_trail = trail_scene.instantiate()
       get_parent().add_child(latest_trail)
-
-    # if deploying != last_deployment_state:
-    #   latest_trail = trail_scene.instantiate()
-    #   get_parent().add_child(latest_trail)
-    #   # if deploying: latest_trail.clear_points()
-    #   last_deployment_state = deploying
-
+      deployed_after_cooldown = true
+    elif Input.is_action_just_released("deploy"):
+      if deployed_after_cooldown:
+        if latest_trail: latest_trail.deactivate()
+        activate_cooldown()
+      deployed_after_cooldown = false
 
 func _physics_process(delta):
-  get_input()
+  rotation_direction = Input.get_axis("left", "right")
+  velocity = transform.y * -1 * speed
   rotation += rotation_direction * rotation_speed * delta
   move_and_slide()
 
 func activate_cooldown() -> void:
   cooling_down = true
+  $Sprite2D.modulate = Color(0.8, 0.8, 0.8, 1.0) # mid-grey
--- scripts/trail.gd
+++ scripts/trail.gd
@@ -1,22 +1,25 @@
 extends Line2D
 
-@export var max_points = 50
+@export var max_points = 100
 @export var min_distance = 5
 
 var plane: CharacterBody2D
-var is_current: bool = true
+var is_active: bool = true
 
 func _ready() -> void:
   clear_points()
   plane = get_parent().get_node("Plane")
 
 func _process(_delta):
-  if plane == null: print("Trail could not find Plane instance!"); return
-  if is_current:
+  if is_active:
+    if plane == null: print("Trail could not find Plane instance!"); return
     var pos = plane.global_position
     if (points.is_empty() or pos.distance_to(points[-1]) > min_distance):
       add_point(pos)
 
     if points.size() > max_points:
-      is_current = false
+      deactivate()
       plane.activate_cooldown()
+
+func deactivate() -> void:
+  is_active = false
