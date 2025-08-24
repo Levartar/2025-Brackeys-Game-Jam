3939b4b98762dd3237bcced70c30f7e81ffbbe39
Fabian Thome <thome.fabian@web.de>
2025-08-25 01:17:10 +0200

    added support for persistent trails

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -2,29 +2,48 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var rotation_speed = 1.5
+@export var cool_down = 2.0
 
 var rotation_direction = 0
 var deploying: bool = false
 var last_deployment_state: bool = false
 var trail: Line2D
+var trail_scene: PackedScene = preload("res://scenes/Trail.tscn")
+var latest_trail: Node
+
+var cooling_down: bool = false
+var current_cool_down: float = 0.0
 
 func _ready() -> void:
-  trail = get_parent().get_node("Trail")
+  current_cool_down = cool_down
 
 func get_input():
   rotation_direction = Input.get_axis("left", "right")
   velocity = transform.y * -1 * speed
-  deploying = Input.is_action_pressed("deploy")
+  deploying = Input.is_action_just_pressed("deploy")
 
-func _process(_delta: float) -> void:
-  if deploying != last_deployment_state:
-    trail.deploying = !trail.deploying
+func _process(delta: float) -> void:
+  if cooling_down:
+    current_cool_down -= delta
+    if current_cool_down <= 0:
+      cooling_down = false
+      current_cool_down = cool_down
+  else:
     if deploying:
-      trail.clear_points()
-    last_deployment_state = deploying
+      latest_trail = trail_scene.instantiate()
+      get_parent().add_child(latest_trail)
+
+    # if deploying != last_deployment_state:
+    #   latest_trail = trail_scene.instantiate()
+    #   get_parent().add_child(latest_trail)
+    #   # if deploying: latest_trail.clear_points()
+    #   last_deployment_state = deploying
 
 
 func _physics_process(delta):
   get_input()
   rotation += rotation_direction * rotation_speed * delta
   move_and_slide()
+
+func activate_cooldown() -> void:
+  cooling_down = true
--- scripts/trail.gd
+++ scripts/trail.gd
@@ -4,20 +4,19 @@ extends Line2D
 @export var min_distance = 5
 
 var plane: CharacterBody2D
-var deploying: bool
+var is_current: bool = true
 
 func _ready() -> void:
   clear_points()
   plane = get_parent().get_node("Plane")
 
 func _process(_delta):
-  if plane == null:
-    return
+  if plane == null: print("Trail could not find Plane instance!"); return
+  if is_current:
+    var pos = plane.global_position
+    if (points.is_empty() or pos.distance_to(points[-1]) > min_distance):
+      add_point(pos)
 
-  var pos = plane.global_position
-
-  if deploying and (points.is_empty() or pos.distance_to(points[-1]) > min_distance):
-    add_point(pos)
-
-  while points.size() > max_points:
-    clear_points()
+    if points.size() > max_points:
+      is_current = false
+      plane.activate_cooldown()
